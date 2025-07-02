cb11b5c95b30cdcdca65c0eabf938d16019b5651
AreejMehboob17 <areej@traversaal.ai>
2025-07-03 00:57:10 +0500

    Initial commit: FastAPI table extraction service

--- app/core/config.py
+++ app/core/config.py
@@ -8,14 +8,11 @@ class Settings(BaseSettings):
 
     load_dotenv()
 
-
-    
     llamaparse_api_key: str = os.getenv("llamaparse_api_key", "")
     unstructured_api_key: str = os.getenv("unstructured_api_key", "")
     openai_api_key:str = os.getenv("openai_api_key", "")
  
 
-    
     class Config:
         case_sensitive = True
         env_file = ".env"
